92b820f17576df752a7956ddea7aa1dcba402da1
leeminchae <lmc.github@gmail.com>
2021-01-27 23:57:42 +0900

    2021-01-27 https://blog.naver.com/opcy02

---
+++ C++ 기초 [13] - this 포인터의 개념/C++ 기초 [13] - this 포인터의 개념/소스2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+class Player
+{
+private:
+	int hp;
+public:
+	void SetHP(int hp)
+	{
+		this->hp = hp;
+	}
+	int GetHP()
+	{
+		return this->hp;
+	}
+};
+
+int main()
+{
+	Player* player = new Player;
+	player->SetHP(10);
+	cout << player->GetHP();
+
+	return 0;
+}
