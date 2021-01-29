a374225eecb056f1604b8a933fe862ee5d640683
leeminchae <lmc.github@gmail.com>
2021-01-29 20:59:59 +0900

    2021-01-29 https://blog.naver.com/opcy02

---
+++ C++ 기초 [22] - for each/C++ 기초 [22] - for each/소스1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int arr[] = { 1,2,3,4,5 };
+	for (int var : arr) {
+		cout << var << endl;
+	}
+	return 0;
+}
